8ea5378be2d5d43aab264f6da68182d981a6f3a2
NatyBilbao <natabilbaocano18@gmail.com>
2024-10-07 17:41:18 -0400

    Exámen commit 1

---
+++ src/test/java/examen1/p1/DateUtilsTest.java
@@ -0,0 +1,22 @@
+package examen1.p1;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DateUtilsTest {
+    DateUtils dateUtils = new DateUtils();
+
+    @ParameterizedTest
+    @CsvSource({
+            "2024-01-01, MONDAY",
+            "2024-02-29, THURSDAY",
+            "2024-03-15, FRIDAY",
+            "2024-08-06, THURSDAY",
+            "2024-12-25, WEDNESDAY"
+    })
+    public void testGetDayOfWeek(String date, String expectedDay) {
+        assertEquals(expectedDay, dateUtils.getDayOfWeek(date));
+    }
+}
